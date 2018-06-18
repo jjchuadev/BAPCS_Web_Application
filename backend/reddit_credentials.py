0fe7ea5b273870271ec469772f0856bc0c371014
John Chua <jjchuadev@gmail.com>
2018-06-18 14:40:08 -0700

    removed credentials

--- backend/reddit_credentials.py
+++
@@ -1,9 +0,0 @@
-class Reddit_Credentials():
-
-    def __init__(self):
-        self.client_id = 'Y0hxDI5iAubSPA'
-        self.client_secret = 'NZEuLXMZnZLjcWsbfpBzUAaNA9Q'
-        self.username = 'BAPCSapp'
-        self.password = 'password'
-        self.redirect_uri = 'https://www.reddit.com/api/v1/authorize?client_id=Y0hxDI5iAubSPA&duration=permanent&redirect_uri=http://localhost:8080&response_type=code&scope=identity&state=...'
-        self.user_agent = 'tutorialv1'
